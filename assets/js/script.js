719db3b1d906e86d4b01a356b33b37cec34f2a67
Saamer Mansoor <i@saamer.me>
2020-06-14 01:23:45 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,3 @@ $(".projects__grid").masonry({
   itemSelector: ".projects__grid-project",
   gutter: 0,
 });
-$(document).ready(function() {
-  $("#emailLnk").click(function()
-   {
-       var email = document.getElementById('email').innerHTML;
-       var body = document.getElementById('body').innerHTML;
-       alert(email+body);
-       document.location.href = "mailto:" + email + "?subject=Interest Form&body=" + body;
-   });
-});​
